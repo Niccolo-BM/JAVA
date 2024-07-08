219ad34bff9f631da30be97decf3b978df71f104
NicolasBernalM <138633064+Niccolo-BM@users.noreply.github.com>
2024-07-07 23:09:50 -0500

    Add files via upload

---
+++ Talle #2/Main/src/Main.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Taller # 2");
+
+    Scanner scanner = new Scanner(System.in);
+
+    // Ejercicio 16: Contraseña segura
+
+        /*System.out.println("Ejercicio 16:");
+        System.out.println("Ingrese una contraseña minimo de 8 caracteres,");
+        System.out.println("que contenga minimo 1 Mayuscula, 1 Minuscula y");
+        System.out.print("1 caracter especial: ");
+
+        string contraseña = scanner.nextLine();
+
+
+
+        boolean esSegura = esContrasenaSegura(contrasena);
+        System.out.println("¿La contraseña es segura? " + esSegura);*/
+
+
+
+
+    // Ejercicio 17: Identificador de carácter
+
+      /*  System.out.println("Ejercicio 17:");
+        System.out.print("Ingrese un carácter de cualquier tipo: ");
+        char caracter = scanner.next().charAt(0);
+
+        string tipo = tipoDeCaracter(caracter);
+        System.out.println("El carácter ingresado es: " + tipo);
+    }
+
+    public string tipoDeCaracter(char caracter) {
+        char caracterMin = Character.toLowerCase(caracter);
+        string tipo = "carácter especial";
+
+        if (caracterMin == 'a' || caracterMin == 'e' || caracterMin == 'i' || caracterMin == 'o' || caracterMin == 'u') {
+            tipo = "vocal";
+        } else if (caracterMin >= 'a' && caracterMin <= 'z') {
+            tipo = "consonante";
+        }return tipo;
+    }
+
+    public static void main(String[] args) {
+        new TipoCaracter().ejecutar();
+
+    scanner.close();
+    }
+}*/
+
+
+
+    scanner.close();
+    }
+}
